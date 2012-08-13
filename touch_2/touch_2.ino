3031850f559bbeaf89c54586778dcb3495d04c4f
Sam Jacoby <sjacoby@media.mit.edu>
2012-08-13 16:29:18 -0400

    This is working on a tiny, moslty

--- touch_2/touch_2.ino
+++ touch_2/touch_2.ino
@@ -4,18 +4,21 @@
 //       (touch input) pb4 -+   +- pb1 (fading while touching)
 //                  ground -+---+- pb0 (fading always)
 
-byte touchPins[] = { PB2, PB3, PB4, PB1 };//, PB5};
-byte ledPins[] = { 
-  PB0};
+#define TIMEOUT 10000
+
+byte touchPins[] ={ 1, 2, 3, 4 };//, PB5};
+byte ledPins[] = { 0 };
 
 int calibration[sizeof(touchPins)];
 
 void setup()
 {
- 
+  
   CLKPR = (1 << CLKPCE);
   CLKPR = 0;
 
+  // Need a minute to get fingers outt've the way. 
+  delay(5000);
   initialize();
   calibrate();
 
@@ -35,21 +38,21 @@ void initialize() {
 }
 
 void calibrate() {
+  
   byte i, j;
-
-  int max = 0;
   
   for(j = 0; j < sizeof(touchPins); j++) {
-    
+
     for (i = 0; i < 8; i++) {
-      
+      digitalWrite(ledPins[0], LOW);
       calibration[j] += chargeTimeR(touchPins[j]);
-      delay(200);
+      delay(5);
+      digitalWrite(ledPins[0], HIGH);
+
     }
   
-  calibration[j] = (calibration[j] + 4 * sizeof(touchPins)) / (8 * sizeof(touchPins));
+    calibration[j] = (calibration[j] + 4) / 8;
   }
-  
   
   digitalWrite(ledPins[0], HIGH);
   delay(500);
@@ -59,15 +62,24 @@ void calibrate() {
 
 void loop()
 {
+  int last;
   byte i, n;
   for(i = 0; i < sizeof(touchPins); i++) {
+    
     n = chargeTimeR(touchPins[i]);
     
     if (n > calibration[i]) {
+    
       digitalWrite(ledPins[0], HIGH);
+      last = millis();
+    
       do {
         delayMicroseconds(500);
         n = chargeTimeR(touchPins[i]);
+        if(millis() - last > TIMEOUT) {
+          calibrate();
+          break;
+        }
       } while (n > calibration[i]);     
       digitalWrite(ledPins[0], LOW);
     } else {
@@ -81,9 +93,9 @@ void loop()
 int chargeTimeR(byte pin) {
   int i, time;
   time = 0;
-  for(i = 0; i < 4; i++) {
+  for(i = 0; i < 24; i++) {
     time += chargeTime(pin);
-    delay(200);
+    delayMicroseconds(500);
   }
   return time;
 }
@@ -91,9 +103,8 @@ int chargeTimeR(byte pin) {
 
 byte chargeTime(byte pin)
 {
-  byte mask = (1 << pin);
-  byte i;
-
+  byte mask, i;
+  mask = digitalPinToBitMask(pin);
   DDRB &= ~mask; // input
   PORTB |= mask; // pull-up on
 
