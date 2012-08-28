3f296a76d179b2a0583d27016851d36b9d46375d
Sam Jacoby <sjacoby@media.mit.edu>
2012-08-28 16:53:14 -0400

    Updated to send bitmask

--- TapClip/TapClip.ino
+++ TapClip/TapClip.ino
@@ -17,6 +17,7 @@
 #define BM1 1
 #define BM2 2
 
+
 // CapSense pins
 int sensePins[] = { A2, A0, A4 };
 int loadPins[] = { A3, A1, A5 };
@@ -29,7 +30,7 @@ CapSense   cs_4_6 = CapSense(A1,A0);
 CapSense   cs_4_8 = CapSense(A5,A4);      
 
 void setup() {
-    int i;
+
     pinMode(PIN1, OUTPUT);
     pinMode(PIN2, OUTPUT);
 
@@ -38,38 +39,56 @@ void setup() {
 }
 
 void loop() {
-    int i;
-    byte touchMask;
+    uint8_t i;
+    byte touchMask = 0x00;
     long totalVal[sizeof(sensePins)];
+    long totalVal_prev[sizeof(sensePins)];
+/*    Serial.println("!!!");
+    Serial.println(sizeof(sensePins));
+    Serial.println(sizeof(int));
+    */
 
     long start = millis();
 
-    long totalVal[0] =  cs_4_2.capSense(30);
-    long totalVal[1] =  cs_4_6.capSense(30);
-    long totalVal[2] =  cs_4_8.capSense(30);
+    totalVal[0] =  cs_4_2.capSense(30);
+    totalVal[1] =  cs_4_6.capSense(30);
+    totalVal[2] =  cs_4_8.capSense(30);
 
 
     //Serial.print(millis() - start);        // check on performance in milliseconds
     //Serial.print(",");                    // tab character for debug windown spacing
-/*
-    Serial.print(total2);                  // print sensor output 1
+    /*
+    Serial.print(totalVal[0]);                  // print sensor output 1
     Serial.print(",");
-    Serial.print(total1);                  // print sensor output 2
+    Serial.print(totalVal[1]);                  // print sensor output 2
     Serial.print(",");
-    Serial.println(total3);                // print sensor output 3
+    Serial.println(totalVal[2]);                // print sensor output 3
     */
 
-    for(i = 0; i++; i < ) {
+
+    for(i = 0; i < (sizeof(sensePins) / sizeof(int)); i++ ) {
+
+        //Serial.println(totalVal[i]);                  // print sensor output 1
+        //Serial.println(i);
+
         if(totalVal[i] > THRESHOLD) {
-            bytemask |= _BV(touchClips[i]);
+
+            touchMask |= _BV(touchClips[i]);
             digitalWrite(PIN1, HIGH);
-        } else {
-            digitalWrite(PIN1, LOW)
+            Serial.println(touchMask);
+
         }
     
     }
 
-    Serial.write(touchMask)
+    if(touchMask != 0) {
+        Serial.write(touchMask);
+        digitalWrite(PIN2, HIGH);
+    } else {
+        digitalWrite(PIN1, LOW);
+    }
+
+    digitalWrite(PIN2, LOW);
 
     delay(10);
 
