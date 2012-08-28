0dad8019375eb895018f25d3b506764f7c0dad59
Sam Jacoby <sjacoby@media.mit.edu>
2012-08-28 16:10:17 -0400

    Updated for better serial.

--- TapClip/TapClip.ino
+++ TapClip/TapClip.ino
@@ -8,42 +8,69 @@
  * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
  */
 
-#define MAX 500
-#define PIN1 8
-#define PIN2 9
+#define THRESHOLD 500
+
+#define PIN1 8 //LED
+#define PIN2 9 //LED
+
+#define BM0 0
+#define BM1 1
+#define BM2 2
+
+// CapSense pins
+int sensePins[] = { A2, A0, A4 };
+int loadPins[] = { A3, A1, A5 };
+
+// Bit values of clips, to bitshift things correctly for serial
+int touchClips[] = { BM0, BM1, BM2 };
 
 CapSense   cs_4_2 = CapSense(A3,A2);        
 CapSense   cs_4_6 = CapSense(A1,A0);       
 CapSense   cs_4_8 = CapSense(A5,A4);      
 
-void setup()                    
-{
-  // cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-   pinMode(PIN1, OUTPUT);
-   pinMode(PIN2, OUTPUT);
-   Serial.begin(9600);
+void setup() {
+    int i;
+    pinMode(PIN1, OUTPUT);
+    pinMode(PIN2, OUTPUT);
+
+    Serial.begin(9600);
+
 }
 
-void loop()                    
-{
+void loop() {
+    int i;
+    byte touchMask;
+    long totalVal[sizeof(sensePins)];
+
     long start = millis();
-    long total1 =  cs_4_2.capSense(30);
-    long total2 =  cs_4_6.capSense(30);
-    long total3 =  cs_4_8.capSense(30);
+
+    long totalVal[0] =  cs_4_2.capSense(30);
+    long totalVal[1] =  cs_4_6.capSense(30);
+    long totalVal[2] =  cs_4_8.capSense(30);
+
 
     //Serial.print(millis() - start);        // check on performance in milliseconds
     //Serial.print(",");                    // tab character for debug windown spacing
-
+/*
     Serial.print(total2);                  // print sensor output 1
     Serial.print(",");
     Serial.print(total1);                  // print sensor output 2
     Serial.print(",");
     Serial.println(total3);                // print sensor output 3
+    */
 
-    if((total1 > MAX)|(total2 > MAX)|(total3 > MAX)) {
-        digitalWrite(PIN1, HIGH);
-    } else {
-        digitalWrite(PIN1, LOW);
+    for(i = 0; i++; i < ) {
+        if(totalVal[i] > THRESHOLD) {
+            bytemask |= _BV(touchClips[i]);
+            digitalWrite(PIN1, HIGH);
+        } else {
+            digitalWrite(PIN1, LOW)
+        }
+    
     }
+
+    Serial.write(touchMask)
+
     delay(10);
+
 }
