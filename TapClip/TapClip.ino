029df0e432fba1665c23aaa1f04cf6c76570a318
Sam Jacoby <sjacoby@media.mit.edu>
2012-09-05 16:35:10 -0400

    Better, now.

--- TapClip/TapClip.ino
+++ TapClip/TapClip.ino
@@ -1,14 +1,15 @@
 #include <CapSense.h>
 
 /*
- * CapitiveSense Library Demo Sketch
- * Paul Badger 2008
+ * TapClip -- as it is known  
+ * 
  * Uses a high value resistor e.g. 10M between send pin and receive pin
  * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
- * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
  */
 
 #define THRESHOLD 500
+#define SENSITIVITY 30
+#define NUMPINS sizeof(sensePins)/sizeof(int)
 
 #define PIN1 8 //LED
 #define PIN2 9 //LED
@@ -16,6 +17,11 @@
 #define BM0 0
 #define BM1 1
 #define BM2 2
+
+#define START  0xfe
+#define END    0xdc
+#define VAL    0x81
+  
 
 
 // CapSense pins
@@ -25,9 +31,9 @@ int loadPins[] = { A3, A1, A5 };
 // Bit values of clips, to bitshift things correctly for serial
 int touchClips[] = { BM0, BM1, BM2 };
 
-CapSense   cs_4_2 = CapSense(A3,A2);        
-CapSense   cs_4_6 = CapSense(A1,A0);       
-CapSense   cs_4_8 = CapSense(A5,A4);      
+CapSense   cs_1 = CapSense(A3,A2);        
+CapSense   cs_2 = CapSense(A1,A0);       
+CapSense   cs_3 = CapSense(A5,A4);      
 
 void setup() {
 
@@ -40,57 +46,38 @@ void setup() {
 
 void loop() {
     uint8_t i;
+    
     byte touchMask = 0x00;
-    long totalVal[sizeof(sensePins)];
-    long totalVal_prev[sizeof(sensePins)];
-/*    Serial.println("!!!");
-    Serial.println(sizeof(sensePins));
-    Serial.println(sizeof(int));
-    */
+    byte actionMask = VAL;
+
+    long totalVal[NUMPINS];
+
+
+    long totalVal_prev[NUMPINS];
 
     long start = millis();
 
-    totalVal[0] =  cs_4_2.capSense(30);
-    totalVal[1] =  cs_4_6.capSense(30);
-    totalVal[2] =  cs_4_8.capSense(30);
+    totalVal[0] =  cs_1.capSense(SENSITIVITY);
+    totalVal[1] =  cs_2.capSense(SENSITIVITY);
+    totalVal[2] =  cs_3.capSense(SENSITIVITY);
 
 
-    //Serial.print(millis() - start);        // check on performance in milliseconds
-    //Serial.print(",");                    // tab character for debug windown spacing
-    /*
-    Serial.print(totalVal[0]);                  // print sensor output 1
-    Serial.print(",");
-    Serial.print(totalVal[1]);                  // print sensor output 2
-    Serial.print(",");
-    Serial.println(totalVal[2]);                // print sensor output 3
-    */
-
-
-    for(i = 0; i < (sizeof(sensePins) / sizeof(int)); i++ ) {
-
-        //Serial.println(totalVal[i]);                  // print sensor output 1
-        //Serial.println(i);
+    for(i = 0; i < NUMPINS; i++ ) {
 
         if(totalVal[i] > THRESHOLD) {
 
-            touchMask |= _BV(touchClips[i]);
+            touchMask |= 1 << touchClips[i];
             digitalWrite(PIN1, HIGH);
 
         }
     
     }
 
-    if(touchMask != 0) {
-        //Serial.write(touchMask);
-        Serial.write(touchMask);
-        Serial.flush();
-        digitalWrite(PIN2, HIGH);
-    } else {
-        digitalWrite(PIN1, LOW);
-    }
+   
+    Serial.write(touchMask);
+    Serial.flush();
+ 
 
-
-    delay(10);
-    digitalWrite(PIN2, LOW);
+    delay(100);
 
 }
