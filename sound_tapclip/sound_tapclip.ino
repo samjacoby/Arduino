721a7fe74de896b7f1564cf7f90b33e010ff210d
Sam Jacoby <sjacoby@media.mit.edu>
2013-01-10 19:24:52 -0500

    Terrible.

--- sound_tapclip/sound_tapclip.ino
+++ sound_tapclip/sound_tapclip.ino
@@ -15,23 +15,30 @@
 
 long sensorRead, maxSensorRead, minSensorRead;
 CapSense c = CapSense(A0, A1);
+
 void setup(){
+
+    sei(); //enable interrupts (default enabled, I think?)
+    TCCR0A |= (1 << COM0A0) | (1 << WGM01); // CTC mode
+
+    // set prescaling on timer
+    TCCR0B |= (1 << CS02) | (1 << CS00); 
+    TIMSK0 |= (1 << OCIE0A);
+
     maxSensorRead = 0;
     minSensorRead = INT_MAX;
     //set digital pins 0-7 as outputs
-    for (int i=0;i<8;i++){
-        pinMode(i,OUTPUT);
-    }
+    DDRD = (1 << 6);
+
     Serial.begin(9600);
 }
 
 long mappedSensorRead;
 int i;
 void loop(){
+/*
 
-
-
-    sensorRead = c.capSense(30);
+    sensorRead = c.capSense(1);
     if(sensorRead > maxSensorRead) {
         maxSensorRead = sensorRead;
     }
@@ -41,20 +48,24 @@ void loop(){
 
     mappedSensorRead = map(sensorRead, minSensorRead, maxSensorRead, 0, 255);
     i = (int) mappedSensorRead;
+    */
+    i = 200;
+
     
-    /*
+   /* 
     for(int i=0; i<255; i++) {
         PORTD = i;
         delayMicroseconds(10);
     }
-    */
-    /*
+   
+    
     for(int i=400; i<800; i++) {
         PORTD = 0;
         delayMicroseconds(i);
         PORTD = 255;
         delayMicroseconds(i);
     }
+    
     for(int i=800; i>400; i--) {
         PORTD = 0;
         delayMicroseconds(i);
@@ -63,7 +74,11 @@ void loop(){
     }
     */
     
-    PORTD = i;
+    //PORTD = i;
         
 }
 
+ISR(TIMER0_COMPA_vect) {
+    OCR0A = i;
+    PORTD ^= 64; 
+}
