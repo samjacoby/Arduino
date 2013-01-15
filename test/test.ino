62a71d029783a28d5c38fdb6960535ada8fc987e
Sam Jacoby <sjacoby@media.mit.edu>
2013-01-15 17:24:54 -0500

    Yo.

--- test/test.ino
+++ test/test.ino
@@ -1,41 +1,34 @@
-//#include <CapSense.h>
+#include <CapSense.h>
 
 #define LED1 A4 
 #define LED2 A5 
 
 void setup() {
 
-    pinMode(LED1, OUTPUT);
-    pinMode(LED2, OUTPUT);
-    //pcm_init();
+    DDRF |= (1 << PINF1) | (1 << PINF0);
 
     DDRD = (1 << PIND0);
-}
-
-void pcm_init() {
-
-    TCCR0A =  (1 << WGM01) |  (1 << WGM00) | (1 << COM0B1);
-
-    // set prescaling on timer
-    TCCR0B = (1 << WGM02) | (1 << CS01) | (1 << CS00);
-
-    // set interrupts (use OCR0A for CTC)
-    //TIMSK0 = (1 << OCIE0B);
-    //
-    OCR0A = 255;
-    OCR0B = 32;
-
+    timer_init();
 
 }
+
+void timer_init() {
+    TCCR1B = (1 << CS12) | (1 << CS10);
+    TIMSK1 = (1 << TOIE1);
+    sei();
+}
+
+
 
 void loop() {
-
     for(int j=1; j<10; j++) {
         for(int i=0; i<255; i++) {
             PORTD ^= (1 << PIND0);        
             delayMicroseconds(i*j);
         }
     }
+}
 
-
+ISR(TIMER1_OVF_vect) {
+    PORTF ^= (1 << PINF0) | (1 << PINF1);
 }
