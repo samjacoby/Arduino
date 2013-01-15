f11d3376c2ff98d8169ea8574e279a6ed4cef7e7
Sam Jacoby <sjacoby@media.mit.edu>
2013-01-15 17:58:30 -0500

    Moving.

--- touch_sound/touch_sound.ino
+++ touch_sound/touch_sound.ino
@@ -7,25 +7,28 @@ void setup() {
 
     DDRF |= (1 << PINF1) | (1 << PINF0);
     DDRD = (1 << PIND0);
-    timer_init();
+    timer0_init();
+    timer1_init();
+    sei();
+}
+
+void timer0_init() {
+    TCCR0B = (1 << CS01) | (1 << CS00);
+    TIMSK0 = 1 << OCIE0B;
 
 }
 
-void timer_init() {
+void timer1_init() {
     TCCR1B = (1 << CS12) | (1 << CS10);
     TIMSK1 = (1 << TOIE1);
-    sei();
 }
 
 
 int i, j;
-void loop() {
-    for(j=1; j<10;j++) {
-        for(i=0; i<255; i++) {
-            PORTD ^= (1 << PIND0);        
-            delayMicroseconds(i*j);
-        }
-    }
+void loop() {}
+
+ISR(TIMER0_COMPB_vect) {
+    PORTD ^= (1 << PIND0);
 }
 
 ISR(TIMER1_OVF_vect) {
