acd6432b20db58a7f2b779738d4ec0b63b28c34d
Sam Jacoby <sjacoby@media.mit.edu>
2013-01-17 18:01:47 -0500

    Working. This sucker is working alright.

--- touch_timer_sound/touch_timer_sound.ino
+++ touch_timer_sound/touch_timer_sound.ino
@@ -3,41 +3,43 @@
 
 CapSense clip_one = CapSense(9, 10);
 CapSense clip_two = CapSense(4, 12);
-//CapSense clip_two = CapSense(13, 5);
+CapSense clip_three = CapSense(13, 5);
 void setup() 
 {
    cli();
    timer1_init();
-   timer0_init();
+//   timer0_init();
    sei();
 
    Serial.begin(115200);
 }
 
-long total1, total2;
+long total1, total2, total3;
 char * data_str = (char*) malloc(18 * sizeof(char));
 void loop()                    
 {
     total1 =  clip_one.capSense(5);
     total2 =  clip_two.capSense(5);
+    total3 =  clip_three.capSense(5);
 
-    sprintf(data_str, "%lu, %lu", total1, total2);
+    sprintf(data_str, "%lu, %lu, %lu", total1, total2, total3);
+    
     Serial.println(data_str);
     delay(10);
+    OCR1A = 0x00f + total1 + total2 + total3; 
 
 }
 
 void timer1_init() {
-    DDRD = (1 << PIND0);
-    TCCR1A = 0x00;
+    DDRD |= (1 << PIND0);
+    TCCR1A = (1 << WGM11) | (1 << WGM10);
     TCCR1B = (1 << WGM13) | (1 << CS10) ;   
-    TIMSK1 = (1 << ICIE1);
-    ICR1 = 0x01;
+    TIMSK1 = (1 << OCIE1A);
+    
 } 
 
-ISR(TIMER1_CAPT_vect) {
+ISR(TIMER1_COMPA_vect) {
     PORTD ^= (1 << PIND0);
-    ICR1 += (int)total1 + (int) total2; ;
 }
 
 void timer0_init() {
